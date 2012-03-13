ac65e06d5206a015b7dc1065651ed3f8d4ac7c20
Dawid Spiechowicz <spiechu@gmail.com>
2012-03-13 18:49:46 +0100

    add license

--- library/Spiechu/TimeSpan/TimeSpan.php
+++ library/Spiechu/TimeSpan/TimeSpan.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+* This file is part of the TimeSpan package.
+*
+* (c) Dawid Spiechowicz <spiechu@gmail.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace Spiechu\TimeSpan;
 
 abstract class TimeSpan {
