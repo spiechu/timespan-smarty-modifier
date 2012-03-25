44a07bbb773fb94251a45f116dc9891730148225
spiechu <spiechu@gmail.com>
2012-03-25 13:41:17 +0200

    simplify modifier

--- plugins/modifier.timespan.php
+++ plugins/modifier.timespan.php
@@ -9,44 +9,18 @@
  * file that was distributed with this source code.
  */
 
-use \DateTime,
-    Spiechu\TimeSpan\AbstractTimeSpan,
-    Spiechu\TimeSpan\TimeSpanEN,
-    Spiechu\TimeSpan\TimeSpanException;
+use Spiechu\TimeSpan\TimeSpan;
 
 /**
  * @param \DateTime|int $startDateTime \DateTime or timestamp to compute date interval
  * @param string $lang language of message; if can't find proper language - falls back on english
  * @param bool $suffix show suffix?
  * @return string 
- * @throws Spiechu\TimeSpan\TimeSpanException when can't set up date
  */
 function smarty_modifier_timespan($startDateTime, $lang = 'EN', $suffix = true) {
-    $className = 'Spiechu\TimeSpan\TimeSpan' . strtoupper($lang);
-    if (class_exists($className)) {
-        $timeSpan = new $className();
-
-        // double check if class extends abstract TimeSpan class
-        if (!($timeSpan instanceof AbstractTimeSpan)) {
-            $timeSpan = new TimeSpanEN();
-        }
-    } else {
-
-        // if unknown language or class doesn't extend AbstractTimeSpan, fall back to english
-        $timeSpan = new TimeSpanEN();
-    }
-
-    if ($startDateTime instanceof DateTime) {
-        $date = $startDateTime;
-
-        // if it's int, assume it's timestamp
-    } elseif (is_int($startDateTime)) {
-        $date = new DateTime();
-        $date->setTimestamp($startDateTime);
-    } else {
-        throw new TimeSpanException('Unknown startDateTime: ' . $startDateTime);
-    }
-
-    $timeSpan->setStartDate($date)->showSuffix($suffix);
+    $timeSpan = new TimeSpan();
+    $timeSpan->setStartDate($startDateTime)
+             ->setLanguage($lang)
+             ->showSuffix($suffix);
     return $timeSpan->getTimeSpan();
 }
